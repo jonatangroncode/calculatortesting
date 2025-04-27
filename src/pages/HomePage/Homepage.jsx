cf57add4aaa357f5b068f72e772249226375950e
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-04-27 22:11:14 +0200

    fetch all movies is loged in and token exist

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -15,13 +15,36 @@ const Homepage = () => {
     if (!token) {
       navigate("/login");
     } else {
-      console.log("Token exists:", token);
+      fetchMovies();
     }
   }, []);
 
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        "https://tokenservice-jwt-2025.fly.dev/movies",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+      setMovies(data);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+  console.log(movies);
+
   return (
     <main>
-      <h1>Välkommen till vår hemsida!</h1>
+      <h1>Välkommen till en hemsida som visar upp dina filmer!</h1>
     </main>
   );
 };
