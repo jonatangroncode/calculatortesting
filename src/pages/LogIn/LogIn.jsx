0bbe0d670fd8fde1347bfce7b5c7249e5b97d09a
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-04-27 21:01:05 +0200

    Added eventhandeling updating state

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,17 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
 const LogIn = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form data:", formData);
+  };
+
   return (
     <main>
       <h1>Logga in</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Användarnamn:</label>
-          <input type="text" id="username" name="username" required />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+            required
+          />
         </div>
         <div>
           <label htmlFor="password">Lösenord:</label>
-          <input type="password" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            required
+          />
         </div>
         <button type="submit">Logga in</button>
       </form>
