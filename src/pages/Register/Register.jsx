5a6c2ba1db49ad811eda9f2cb71112ec3d0b2f2a
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-04-27 18:23:33 +0200

    Form logic to register a user

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,21 +1,70 @@
 import React from "react";
+import { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    role: "",
+    userConsent: {
+      isTermsAndAgreementsConsented: true,
+      timestamp: new Date().toISOString(),
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div>
       <main>
         <h1>Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="username">Username: </label>
+            <label
+              type="text"
+              id="username"
+              name="username"
+              htmlFor="username"
+              value={formData.username}
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
+              required
+            >
+              Username:{" "}
+            </label>
             <input type="text" id="username" name="username" required />
           </div>
           <div>
-            <label htmlFor="password">Password: </label>
+            <label
+              type="password"
+              id="password"
+              name="password"
+              htmlFor="password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              required
+            >
+              Password:{" "}
+            </label>
             <input type="password" id="password" name="password" required />
           </div>
           <div>
-            <select name="role" id="role">
+            <select
+              name="role"
+              id="role"
+              value={formData.role}
+              onChange={(e) =>
+                setFormData({ ...formData, role: e.target.value })
+              }
+              required
+            >
+              <option value="">Välj roll</option>
               <option value="GUEST">Guest</option>
               <option value="USER">User</option>
               <option value="ADMIN">Admin</option>
